604de730256f9d435a44ba86104d916b7a9cf966
vardhaman16 <coderbro1990@gmail.com>
2020-02-05 17:15:06 +0530

    port chage

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
+var server = http.createServer(function (request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.writeHead(200, { "Content-Type": "text/plain" });
     response.end("Hello World IBM!");
 
 });
 
-var port = 80;
+var port = 9080;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
